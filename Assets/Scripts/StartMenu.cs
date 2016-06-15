5b9bec090152fdaa7044463b83b34ea10c6ebec0
evening424 <249866765@qq.com>
2016-06-15 16:45:24 +0800

    1

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+    public MovieTexture movTexture;
+
+	public bool isDrawMov = true;
+
+	public TweenScale logoTween;
+
+	// Use this for initialization
+	void Start () {
+        movTexture.loop = false;
+        movTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (isDrawMov) {
+			if(Input.GetMouseButton(0)){
+				StopMov();
+			}
+		}
+	}
+
+    void OnGUI() {
+		if (isDrawMov) {
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movTexture);
+		}
+    }
+
+	private void StopMov(){
+		movTexture.Stop ();
+		isDrawMov = false;
+		//
+		logoTween.PlayForward ();
+	}
+}
