6061aeef6b1057c3dbbe7eb8c758ceb6c39cc060
evening424 <249866765@qq.com>
2016-06-15 11:01:11 +0800

    8

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+    public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+        movTexture.loop = false;
+        movTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnGUI() {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movTexture);
+    }
+}
