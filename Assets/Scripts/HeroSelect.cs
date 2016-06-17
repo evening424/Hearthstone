1593275447660e6ce276358a0899b3e0875f0875
evening424 <249866765@qq.com>
2016-06-17 15:47:45 +0800

    0

---
+++ Assets/Scripts/HeroSelect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroSelect : MonoBehaviour {
+	public int heroId = 0;
+	
+	private string[] heroNames = {
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9"
+	};
+
+	private UISprite selectHeroImage;
+	private UILabel selectHeroName;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void Awake(){
+		selectHeroImage = this.transform.parent.Find ("hero0").GetComponent<UISprite> ();
+		selectHeroName = this.transform.parent.Find ("hero_name").GetComponent<UILabel> ();
+	}
+	
+	void OnClick(){
+		string heroname = this.gameObject.name;
+		selectHeroImage.spriteName = heroname;
+		selectHeroName.text = heroNames [heroId-1];
+	}
+}
