ffa2f23d8b58b444ebd839fb4f2326d9ac109c39
evening424 <249866765@qq.com>
2016-06-17 17:16:20 +0800

    1

---
+++ Assets/Blankmask.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blankmask : MonoBehaviour {
+
+	public static Blankmask _instance;
+	void Awake(){
+		_instance = this;
+		this.gameObject.SetActive (false);
+	}
+
+	public void Show(){
+		this.gameObject.SetActive (true);
+	}
+
+	public void Hide(){
+		this.gameObject.SetActive (false);
+	}
+}
---
+++ Assets/vsshow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vsshow : MonoBehaviour {
+
+	public static vsshow _instance;
+	void Awake(){
+		_instance = this;
+	}
+
+	public TweenScale vsTween;
+	public TweenPosition hero1Tween;
+	public TweenPosition hero2Tween;
+
+	// Use this for initialization
+	void Start () {
+		Show("hero1", "hero2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Show(string hero1Name , string hero2Name){
+		Blankmask._instance.Show ();
+
+		hero1Tween.GetComponent<UISprite> ().spriteName = hero1Name;
+		hero2Tween.GetComponent<UISprite> ().spriteName = hero2Name;
+
+		vsTween.PlayForward ();
+		hero1Tween.PlayForward ();
+		hero2Tween.PlayForward ();
+	}
+}
